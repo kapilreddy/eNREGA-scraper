b0f64f0119eb1b0458dcce422961b0ad3f22b0b6
Kapil Reddy <kapil@infinitelybeta.com>
2011-08-12 00:07:47 +0530

    remove gevent code

--- mainscraper.py
+++ mainscraper.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
 
 import yajl as json
-import gevent
-from gevent import local, monkey
 from district_scraper import districtExtract
 from taluka_scraper import talukaExtract
 from panchayat_scraper import panchayatExtract
 import time
 
-monkey.patch_all()
 
 import urllib
 from urlparse import urlparse
@@ -30,41 +27,29 @@ def _cleanUrl(url):
     return encoded_url
 
 
-def fetch_panchayat(url, district, taluka):
+def fetch_panchayat(url, taluka):
     encoded_url = _cleanUrl(url)
-    data = panchayatExtract(encoded_url)
-    if "panchayat" in global_data[district]["taluka"][taluka].keys():
-        global_data[district]["taluka"][taluka]["panchayat"] =\
-            dict(data.items() +
-                 global_data[district]["taluka"][taluka]["panchayat"].items())
-    else:
-        global_data[district]["taluka"][taluka]["panchayat"] = data
+    result = panchayatExtract(encoded_url)
+    return result
 
 
 def fetch_taluka(url, district):
     encoded_url = _cleanUrl(url)
-    data = talukaExtract(encoded_url)
-    if "taluka" in global_data[district].keys():
-        global_data[district]["taluka"] =\
-        dict(data.items() + global_data[district]["taluka"].items())
-    else:
-        global_data[district]["taluka"] = data
-    jobs = [gevent.spawn(fetch_panchayat, value['url'], district, key)
-            for key, value in data.iteritems()]
-    gevent.joinall(jobs)
+    result = talukaExtract(encoded_url)
+    print result
+    for key, value in result.iteritems():
+        result[key]["panchayat"] = fetch_panchayat(value['url'], key)
+    return result
 
 
-global_data = local.local()
-data = districtExtract("http://164.100.112.66/netnrega/writereaddata/citizen"\
+result = districtExtract("http://164.100.112.66/netnrega/writereaddata/citizen"\
                             "_out/phy_fin_reptemp_Out_18_local_1112.html")
-global_data = data
-jobs = [gevent.spawn(fetch_taluka,
-                     value["url"],
-                     key)
-        for key, value in data.iteritems()]
-gevent.joinall(jobs)
+
+for key, value in result.iteritems():
+    result[key]["taluka"] = fetch_taluka(value["url"], key)
+
 
 f = open('database/data.json', 'w')
-output = json.dumps(global_data)
+output = json.dumps(result)
 print time.time() - start
 f.write(output)
